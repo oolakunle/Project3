ffab9bc83d48a6562d51a6747699cc9fda9ab437
Abdulmajeed Yusuf <oolakunle@ymail.com>
2018-05-18 12:00:06 +1000

    one more

--- config/routes.rb
+++ config/routes.rb
@@ -54,5 +54,4 @@ Rails.application.routes.draw do
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
-  get '/logout' => 'pages#home'
 end
