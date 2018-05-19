aa45dd8ead49cdbb495402a80241cc000952a8b3
Abdulmajeed Yusuf <oolakunle@ymail.com>
2018-05-19 11:29:11 +1000

    fixed user show

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       puts 'firs if statement'
-      if user.activated
+      if user.activated?
         puts 'hello'
         log_in user
         params[:session][:remember_me] == '1' ? remember(user) : forget(user)
