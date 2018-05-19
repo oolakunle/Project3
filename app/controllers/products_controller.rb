7074a22447810129dd21848a7502b5829bd3b6de
Abdulmajeed Yusuf <oolakunle@ymail.com>
2018-05-19 12:49:31 +1000

    fixed cloudinary perfectly

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -25,14 +25,17 @@ class ProductsController < ApplicationController
   # POST /products.json
   def create
     @product = Product.new(product_params)
-
-    respond_to do |format|
-      if @product.save
-        format.html { redirect_to @product, notice: 'Product was successfully created.' }
-        format.json { render :show, status: :created, location: @product }
-      else
-        format.html { render :new }
-        format.json { render json: @product.errors, status: :unprocessable_entity }
+    if params[:product][:image].present?
+      cloudinary = Cloudinary::Uploader.upload( params[ "product" ][ "image" ] )
+      @product.update :image => cloudinary["url"]
+      respond_to do |format|
+        if @product.save
+          format.html { redirect_to @product, notice: 'Ad was successfully posted.' }
+          format.json { render :show, status: :created, location: @product }
+        else
+          format.html { render :new }
+          format.json { render json: @product.errors, status: :unprocessable_entity }
+        end
       end
     end
   end
@@ -42,7 +45,7 @@ class ProductsController < ApplicationController
   def update
     respond_to do |format|
       if @product.update(product_params)
-        format.html { redirect_to @product, notice: 'Product was successfully updated.' }
+        format.html { redirect_to @product, notice: 'Ad was successfully updated.' }
         format.json { render :show, status: :ok, location: @product }
       else
         format.html { render :edit }
@@ -56,7 +59,7 @@ class ProductsController < ApplicationController
   def destroy
     @product.destroy
     respond_to do |format|
-      format.html { redirect_to products_url, notice: 'Product was successfully destroyed.' }
+      format.html { redirect_to products_url, notice: 'Ad was successfully deleted.' }
       format.json { head :no_content }
     end
   end
@@ -69,6 +72,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:title, :description, :image, :suburb, :category_id)
+      params.require(:product).permit(:title, :description, :suburb, :category_id)
     end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      puts 'firs if statement'
       if user.activated?
-        puts 'hello'
         log_in user
         params[:session][:remember_me] == '1' ? remember(user) : forget(user)
         redirect_back_or user
