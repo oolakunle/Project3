a4ef503f5773d1bca0fbaed69c8cc0072060ba81
Abdulmajeed Yusuf <oolakunle@ymail.com>
2018-05-19 11:27:29 +1000

    fixed user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    redirect_to root_url
+    redirect_to root_url and return unless true
   end
 
   def new
